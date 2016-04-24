60538e11e1ead01750d969e591daf4f2f2bc8a88
doktordirk <doktordirk@outlook.de>
2016-04-24 13:26:42 +0200

    feat(project): add user and make customer a related model

--- client/src/app.js
+++ client/src/app.js
@@ -1,10 +1,11 @@
 export class App {
   configureRouter(config, router) {
-    config.title = 'Aurelia-Api-Loopback';
+    config.title = 'Aurelia-Orm-Loopback';
     config.map([
       { route: ['', 'welcome'], name: 'welcome',      moduleId: 'welcome',      nav: true, title: 'Welcome' },
       { route: 'users',         name: 'users',        moduleId: 'users',        nav: true, title: 'Github Users' },
-      { route: 'customer',      name: 'customer',     moduleId: 'modules/customer/index', nav: true, title: 'Customers' }
+      { route: 'customers',     name: 'customers',    moduleId: 'customers',              nav: true,  title: 'List Customers' },
+      { route: 'customer',      name: 'customer',     moduleId: 'modules/customer/index', nav: true, title: 'Manage Customers' }
     ]);
 
     this.router = router;
