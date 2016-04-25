8c9477a33222de55bd3c986801cba812d2850be8
doktordirk <doktordirk@outlook.de>
2016-04-25 14:40:01 +0200

    feat(client): remove github users

--- client/src/app.js
+++ client/src/app.js
@@ -2,9 +2,9 @@ export class App {
   configureRouter(config, router) {
     config.title = 'Aurelia-Orm-Loopback';
     config.map([
-      { route: ['', 'welcome'], name: 'welcome',      moduleId: 'welcome',      nav: true, title: 'Welcome' },
-      { route: 'users',         name: 'users',        moduleId: 'users',        nav: true, title: 'Github Users' },
-      { route: 'customers',     name: 'customers',    moduleId: 'customers',              nav: true,  title: 'List Customers' },
+      { route: ['', 'welcome'], name: 'welcome',      moduleId: 'welcome',                nav: true, title: 'Welcome' },
+      { route: 'users',         name: 'users',        moduleId: 'modules/users/profile',  nav: true, title: 'Profile' },
+      { route: 'customers',     name: 'customers',    moduleId: 'customers',              nav: true, title: 'List Customers' },
       { route: 'customer',      name: 'customer',     moduleId: 'modules/customer/index', nav: true, title: 'Manage Customers' }
     ]);
 
